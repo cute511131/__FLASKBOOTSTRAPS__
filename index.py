e19fe2f34ef333893f2a2e494019f0e56f6d9aa2
cute511131 <cute511131@gmail.com>
2023-10-05 15:30:05 +0800

    2023.10.05

--- index.py
+++ index.py
@@ -5,4 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.jinja.html")
+    names=['小可愛','長頸鹿']
+    return render_template("index.jinja.html",names=names)
+@app.route("/kiki")
+def kiki():
+    return render_template("kiki.jinja.html")
+@app.route("/lulu")
+def lulu():
+    return render_template("lulu.jinja.html")
+@app.route("/dayday")
+def dayday():
+    return render_template("dayday.jinja.html")
